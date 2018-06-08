6a18dec3df1cb468d1d78bcf5af9c2ed1e382909
summercake@qq.com <summercake@qq.com>
2018-06-08 00:11:16 -0500

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 $api -> version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function ($api){
 
     $api -> group([
@@ -23,6 +24,14 @@ $api -> version('v1', [
         'limit'      => config('api.rate_limits.sign.limit'),
         'expires'    => config('api.rate_limits.sign.expires'),
     ], function ($api){
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
         // 短信验证码
         $api -> post('verificationCodes', 'VerificationCodesController@store')
              -> name('api.verificationCodes.store');
